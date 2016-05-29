d18b29d7fe9419066f29f5aa35804809e953a158
Luciano Prestes Cavalcanti <lucianopcbr@gmail.com>
2016-05-29 13:09:21 -0300

    Update send_popcon_entries script
    - Check directories
    - Dont remove destiny dir if its already exists

--- send_popcon_entries.sh
+++ send_popcon_entries.sh
@@ -1,8 +1,44 @@
-FOLDER=$1
-SELF_FOLDER=`pwd`
+if [ "$#" -ne  2 ]; then
+    echo "Usage $0 [actual_submissions_folder] [destiny_submissions_folder]"
+    exit 1
+fi
+if ! [ -e "$1" ]; then
+    echo "$1 not found"
+    echo ""
+    echo "Usage $0 [actual_submissions_folder] [destiny_submissions_folder]"
+    exit 1
+fi
+if ! [ -d "$1" ]; then
+    echo "$1 not a directory"
+    echo ""
+    echo "Usage $0 [actual_submissions_folder] [destiny_submissions_folder]"
+    exit 1
+fi
+if ! [ -e "$2" ]; then
+    echo "$2 not found"
+    echo ""
+    echo "Usage $0 [actual_submissions_folder] [destiny_submissions_folder]"
+    exit 1
+fi
+if ! [ -d "$2" ]; then
+    echo "$2 not a directory"
+    echo ""
+    echo "Usage $0 [actual_submissions_folder] [destiny_submissions_folder]"
+    exit 1
+fi
 
-mkdir -p $FOLDER
-for popcon_file in "$SELF_FOLDER"/*popcon
+ACTUAL_SUBMISSIONS_FOLDER=$1
+DESTINY_SUBMISSIONS_FOLDER=$2
+
+for popcon_file in "$ACTUAL_SUBMISSIONS_FOLDER"/*popcon
 do
-  cp $popcon_file $FOLDER
+  dir_name=$(basename "$popcon_file")
+  dir_name=${dir_name:0:2}
+  dir_name=$DESTINY_SUBMISSIONS_FOLDER$dir_name
+
+  if [ ! -d "$dir_name" ]; then
+    mkdir $dir_name
+  fi
+
+  cp $popcon_file $dir_name
 done
